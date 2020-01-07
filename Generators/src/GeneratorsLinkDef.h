5c30f2788e3661b42050311457981bea7cf55d64
Roberto Preghenella <preghenella@bo.infn.it>
2020-01-07 22:46:27 +0100

    [WIP] Pythia6 generator compliant with o2::eventgen::Generator protocol

--- Generators/src/GeneratorsLinkDef.h
+++ Generators/src/GeneratorsLinkDef.h
@@ -30,7 +30,11 @@
 #ifdef GENERATORS_WITH_HEPMC3
 #pragma link C++ class o2::eventgen::GeneratorHepMC + ;
 #endif
-#pragma link C++ class o2::eventgen::Pythia6Generator + ;
+#ifdef GENERATORS_WITH_PYTHIA6
+#pragma link C++ class o2::eventgen::GeneratorPythia6 + ;
+#pragma link C++ class o2::eventgen::GeneratorPythia6Param + ;
+#pragma link C++ class o2::conf::ConfigurableParamHelper < o2::eventgen::GeneratorPythia6Param > + ;
+#endif
 #ifdef GENERATORS_WITH_PYTHIA8
 #pragma link C++ class o2::eventgen::GeneratorPythia8 + ;
 #pragma link C++ class o2::eventgen::GeneratorPythia8Param + ;
